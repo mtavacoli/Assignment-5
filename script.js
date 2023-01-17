ab741e8793bed1342b4d79ebfcfa193538d9855d
Matin Tavacoli <mtavacoli33@gmail.com>
2023-01-17 12:18:04 -0600

    few changes

--- script.js
+++ script.js
@@ -3,25 +3,19 @@ var todayDate = moment().format("dddd, MMM Do YYYY");
 $("#currentDay").html(todayDate);
 
 $(document).ready(function () {
-  // saveBtn click listener
   $(".saveBtn").on("click", function () {
-    // Get nearby values of the description in JQuery
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
-    // Save text in local storage
     localStorage.setItem(time, text);
   });
 
   function timeTracker() {
-    //get current number of hours.
     var timeNow = moment().hour();
 
-    // loop over time blocks
     $(".time-block").each(function () {
       var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-      // To check the time and add the classes for background indicators
       if (blockTime < timeNow) {
         $(this).removeClass("future");
         $(this).removeClass("present");
